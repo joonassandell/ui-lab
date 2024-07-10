2d9e1ed7c87dd768fca11407eadf1b3f6cb1e52b
Joonas Sandell <me@joonassandell.com>
2024-07-11 02:12:18 +0300

    Finish essential credit card styles, tweak shadows

--- app/dynamic-pay-button/DynamicPayButton.tsx
+++ app/dynamic-pay-button/DynamicPayButton.tsx
@@ -84,7 +84,7 @@ export const DynamicPayButton = () => {
       {content && (
         <m.div
           animate={open ? 'open' : 'closed'}
-          className={cn('top-14 w-96 p-3 pt-0', { absolute: !open })}
+          className={cn('w-96 p-3 pt-0', { absolute: !open, 'top-14': !open })}
           initial="closed"
           style={{
             originY: 'top',
@@ -109,16 +109,6 @@ export const DynamicPayButton = () => {
           }}
         >
           <Card />
-          {/* <div
-            className={cn(
-              'shadow-l h-48 rounded-xl border p-3 dark:border-zinc-700 dark:bg-zinc-800',
-            )}
-          >
-            Labore aliqua sint ullamco amet. Mollit duis id eu. Aliquip dolor
-            est non sit laborum excepteur. Cillum irure nostrud irure. Eu
-            aliquip reprehenderit in incididunt officia et dolor occaecat ipsum
-            et fugiat exercitation commodo.
-          </div> */}
         </m.div>
       )}
     </AnimateDimension>
@@ -126,27 +116,38 @@ export const DynamicPayButton = () => {
 };
 
 const Card = () => (
-  <div className={cn('card font-cc h-[200px]')}>
+  <div
+    className={cn(
+      'card text-shadow-black/40 h-52 text-white text-shadow [perspective:1000px]',
+    )}
+  >
     <div
       className={cn(
-        'card-inner h-full w-full transition-transform duration-[600ms] ease-[ease]',
+        'card-inner h-full w-full transition-transform duration-[600ms] ease-[ease] [transform-style:preserve-3d]',
       )}
     >
-      {/* Front */}
       <div
         className={cn(
-          'card-front relative top-0 flex h-full flex-col justify-end gap-3 overflow-hidden rounded-xl p-5',
+          'relative flex h-full flex-col justify-end gap-3 overflow-hidden rounded-xl p-5 font-cc',
+          'shadow-sm shadow-black/30 dark:shadow-black/50',
+          'bg-gradient-to-br from-[#0860bf] to-[#01adef]',
+          'dark:from-[hsl(210,60%,20%)] dark:to-[hsl(210,60%,10%)]',
+          'before:pointer-events-none before:absolute before:inset-0 before:z-[1] before:rounded-xl',
+          'before:shadow-[0_-1px_0_0_hsla(0,0%,0%,0.1)_inset,0_0_0_1px_hsla(0,0%,0%,0.1)_inset]',
+          'dark:before:shadow-[0_1px_0_0_hsla(0,0%,100%,0.06)_inset,0_0_0_1px_hsla(0,0%,100%,0.05)_inset]',
         )}
       >
-        {/* Front background */}
         <div
           className={cn(
-            "card-bg absolute -top-5 right-[-120px] h-[250px] w-[380px] rounded-tl-[100%] before:absolute before:-right-20 before:-top-5 before:h-[250px] before:w-[380px] before:rounded-tl-[100%] before:content-[''] after:absolute after:-top-5 after:right-[-120px] after:h-[250px] after:w-[380px] after:rounded-tl-[100%] after:content-['']",
+            'card-bg absolute -right-24 -top-5 bottom-0 w-full rounded-tl-[100%]',
+            'bg-gradient-to-tl from-[#01adef] to-[#0860bf]',
+            'dark:from-[hsl(210,60%,24%)] dark:to-[hsl(210,60%,9%)] dark:to-95%',
+            'before:absolute before:-right-20 before:h-full before:w-full before:rounded-tl-[100%] before:bg-gradient-to-tl',
+            'after:absolute after:-right-36 after:h-full after:w-full after:rounded-tl-[100%] after:bg-gradient-to-tl',
           )}
         />
         <svg
-          className={cn('absolute right-[25px] top-[30px]')}
-          fill="none"
+          className={cn('absolute right-5 top-6')}
           height="24"
           viewBox="0 0 72 24"
           width="72"
@@ -155,61 +156,70 @@ const Card = () => (
           <path
             clipRule="evenodd"
             d="M52.3973 1.01093L51.5588 5.99054C49.0448 4.56717 43.3231 4.23041 43.3231 6.85138C43.3231 7.89285 44.6177 8.60913 46.178 9.47241C48.5444 10.7817 51.5221 12.4291 51.5221 16.062C51.5221 21.8665 45.4731 24 41.4645 24C37.4558 24 34.8325 22.6901 34.8325 22.6901L35.7065 17.4848C38.1115 19.4688 45.4001 20.032 45.4001 16.8863C45.4001 15.5645 43.9656 14.785 42.3019 13.8811C40.0061 12.6336 37.2742 11.1491 37.2742 7.67563C37.2742 1.30988 44.1978 0 47.1132 0C49.8102 0 52.3973 1.01093 52.3973 1.01093ZM66.6055 23.6006H72L67.2966 0.414276H62.5732C60.3923 0.414276 59.8612 2.14215 59.8612 2.14215L51.0996 23.6006H57.2234L58.4481 20.1566H65.9167L66.6055 23.6006ZM60.1406 15.399L63.2275 6.72235L64.9642 15.399H60.1406ZM14.7942 16.3622L20.3951 0.414917H26.7181L17.371 23.6012H11.2498L6.14551 3.45825C2.83215 1.41281 0 0.807495 0 0.807495L0.108643 0.414917H9.36816C11.9161 0.414917 12.1552 2.50314 12.1552 2.50314L14.1313 12.9281L14.132 12.9294L14.7942 16.3622ZM25.3376 23.6006H31.2126L34.8851 0.414917H29.0095L25.3376 23.6006Z"
-            fill="white"
+            fill="currentColor"
             fillRule="evenodd"
-          ></path>
+          />
         </svg>
-        <div className={cn('absolute right-[15px] top-[55px] scale-50')}>
+        <div
+          className={cn('absolute right-5 top-1 mt-14 origin-right scale-50')}
+        >
           <svg height="56" width="46" xmlns="http://www.w3.org/2000/svg">
             <path
               d="m35,3a50,50 0 0,1 0,50M24,8.5a39,39 0 0,1 0,39M13.5,13.55a28.2,28.5
   0 0,1 0,28.5M3,19a18,17 0 0,1 0,18"
               fill="none"
-              stroke="#f9f9f9"
+              stroke="currentColor"
               strokeLinecap="round"
               strokeWidth="6"
-            ></path>
+            />
           </svg>
         </div>
         <div
           className={cn(
-            'relative grid h-10 w-12 place-content-center rounded bg-[linear-gradient(to_bottom_left,_#ffecc7,_#d0b978)]',
+            'relative mb-3 grid h-10 w-12 place-content-center rounded',
+            'bg-gradient-to-bl from-[#ffecc7] to-[#d0b978]',
           )}
         >
-          <div className={cn('absolute top-[0.81rem] h-px w-full bg-[#333]')} />
-          <div className={cn('absolute top-[1.25rem] h-px w-full bg-[#333]')} />
-          <div className={cn('absolute top-[1.75rem] h-px w-full bg-[#333]')} />
+          <div className={cn('absolute top-[0.8rem] h-px w-full bg-[#333]')} />
+          <div className={cn('absolute top-[1.21rem] h-px w-full bg-[#333]')} />
+          <div className={cn('absolute top-[1.6rem] h-px w-full bg-[#333]')} />
           <div className={cn('absolute left-1/2 h-full w-px bg-[#333]')} />
           <div
             className={cn(
-              'z-[1] h-6 w-5 rounded border border-[#333] bg-[linear-gradient(to_bottom_left,_#efdbab,_#e1cb94)]',
+              'z-[1] h-6 w-5 rounded border border-[#333]',
+              'bg-gradient-to-bl from-[#efdbab] to-[#e1cb94]',
             )}
           />
         </div>
-        <div className={cn('z-[1] flex justify-between gap-4 text-xl')}>
+        <div className={cn('z-[1] flex justify-between gap-5 text-xl')}>
           <div>1234</div>
           <div>5678</div>
           <div>9000</div>
           <div>1234</div>
         </div>
-        <div className={cn('z-[1] text-sm')}>John Doe</div>
-        <div className={cn('absolute bottom-5 right-4 text-sm')}>12/28</div>
+        <div className={cn('z-[1] flex justify-between text-sm uppercase')}>
+          <div>Mr Joonas Sandell</div>
+          <div>12/28</div>
+        </div>
       </div>
-      {/* Back */}
       <div
         className={cn(
-          "card-back absolute top-0 z-[2] flex h-full w-[180%] w-full flex-col justify-between overflow-hidden rounded-[10px] p-4 pb-3 before:absolute before:left-[20%] before:top-[40%] before:h-[120%] before:rounded-[100%] before:bg-[linear-gradient(_to_right_top,#a3d4e7,#a7d5e6,#abd5e4,#aed6e3,#b2d6e2,#aed4e2,#abd3e1,#a7d1e1,#9bcee1,#8ecae1,#81c7e1,#73c3e1_)] before:opacity-[0.15] before:blur-[10px] before:content-[''] after:absolute after:left-0 after:right-0 after:top-5 after:h-10 after:bg-black",
+          'absolute top-0 z-[2] flex h-full w-full flex-col justify-between rounded-xl p-5 pb-3 [backface-visibility:hidden] [transform:rotateY(180deg)]',
+          'bg-gradient-to-br from-[#0860bf] to-[#0192df]',
+          'dark:from-[hsl(210,60%,20%)] dark:to-[hsl(210,60%,10%)]',
+          'before:pointer-events-none before:absolute before:inset-0 before:rounded-xl',
+          'before:shadow-[0_-1px_0_0_hsla(0,0%,0%,0.1)_inset,0_0_0_1px_hsla(0,0%,0%,0.1)_inset]',
+          'dark:before:shadow-[0_1px_0_0_hsla(0,0%,100%,0.06)_inset,0_0_0_1px_hsla(0,0%,100%,0.05)_inset]',
+          'after:absolute after:left-0 after:right-0 after:top-5 after:h-10 after:bg-black',
         )}
       >
         <div
           className={cn(
-            'relative top-2 mt-16 h-9 w-full rounded bg-white p-3 text-right text-[#000]',
+            'relative top-1 mt-16 h-9 w-full rounded bg-white p-3 text-right text-[#000]',
           )}
         >
           <label
-            className={cn(
-              'text-2xs mx-0 -mt-8 mb-2 block uppercase text-white',
-            )}
+            className={cn('-mt-8 mb-2 block text-2xs uppercase text-white')}
           >
             ccv
           </label>
