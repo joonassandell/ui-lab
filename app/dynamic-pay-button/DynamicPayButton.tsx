9b137a9dacf7e8bc42439012669c5a90cc0e8df6
Joonas Sandell <me@joonassandell.com>
2024-07-19 15:34:59 +0300

    Simplify content opening logic, fix focus scroll
    - Use AnimatePresence to handle the hiding of the content. This makes the manual state visibility handling unnecessary.
    - Use the variant definitions instead of the previous "animating" state to detect the complete/start animation direction
    - Scrolling with the focus f***ed the content animation if tabs were clicked fast, so prevent the scroll

--- app/dynamic-pay-button/DynamicPayButton.tsx
+++ app/dynamic-pay-button/DynamicPayButton.tsx
@@ -24,10 +24,8 @@ import { useEffect, useRef, useState } from 'react';
  */
 export const DynamicPayButton = () => {
   const [open, setOpen] = useState(false);
-  const [animating, setAnimating] = useState(false);
   const [switchCard, setSwitchCard] = useState(false);
   const [ccv, setCcv] = useState<string>('');
-  const [content, setContent] = useState(false);
   const [overflow, setOverflow] = useState(false);
   const [icon, setIcon] = useState(<CreditCard />);
   const [selectedTab, setSelectedTab] = useState(TABS[0].label);
@@ -36,9 +34,7 @@ export const DynamicPayButton = () => {
 
   const handleOpen = () => {
     setOpen(!open);
-    setAnimating(true);
     setOverflow(false);
-    setContent(true);
     setIcon(open ? <CreditCard /> : <Close />);
     !open && setSelectedTab(TABS[0].label);
     !open && setCcv('');
@@ -73,17 +69,18 @@ export const DynamicPayButton = () => {
           'overflow-visible': overflow,
         },
       )}
-      onAnimationComplete={() => {
-        !open && !animating && setContent(false);
-        open && !animating && inputRef.current?.focus();
+      onAnimationComplete={variant => {
+        variant === 'open' &&
+          inputRef.current?.focus({
+            preventScroll: true,
+          });
+        variant === 'closed' &&
+          buttonRef.current?.focus({
+            preventScroll: true,
+          });
       }}
-      onAnimationStart={() =>
-        open ? buttonRef.current?.blur() : buttonRef.current?.focus()
-      }
-      onUpdate={e => {
-        if (e.borderRadius === 20 || e.borderRadius === 60) {
-          animating && setAnimating(false);
-        }
+      onAnimationStart={variant => {
+        variant === 'open' && buttonRef.current?.blur();
       }}
       variants={{
         closed: { borderRadius: 60 },
@@ -141,7 +138,7 @@ export const DynamicPayButton = () => {
               'hover:text-zinc-800',
               'dark:hover:text-zinc-100',
               {
-                'outline-0': !open || (animating && open),
+                'outline-0': !open,
                 'px-1': open,
               },
             )}
@@ -165,94 +162,97 @@ export const DynamicPayButton = () => {
             </AnimatePresence>
           </m.button>
         </header>
-        {content && (
-          <m.div
-            animate={open ? 'open' : 'closed'}
-            className={cn('w-[368px] p-3 md:w-96', {
-              absolute: !open,
-              'top-14': !open,
-            })}
-            initial="closed"
-            style={{ originY: 'bottom' }}
-            transition={{
-              ...TRANS_SPRING,
-              filter: { delay: 0.2 },
-            }}
-            variants={{
-              closed: {
-                filter: 'blur(4px)',
-                opacity: 0,
-                rotate: 20,
-                scale: 0.2,
-              },
-              open: {
-                filter: 'blur(0px)',
-                opacity: 1,
-                rotate: 0,
-                scale: 1,
-              },
-            }}
-          >
-            <Tabs.Content value={TABS[0].label}>
-              <Cards
-                ccv={ccv}
-                overflow={overflow}
-                setCcv={setCcv}
-                setOverflow={setOverflow}
-                setSwitchCard={setSwitchCard}
-                switchCard={switchCard}
-              />
-              <footer
-                className={cn(
-                  'flex w-full items-center justify-between gap-2 pt-4',
-                )}
-              >
-                <button
-                  aria-label="Switch active credit card"
+        <AnimatePresence mode="popLayout">
+          {open && (
+            <m.div
+              animate="open"
+              className={cn('w-[368px] p-3 md:w-96', {
+                absolute: !open,
+                'top-14': !open,
+              })}
+              exit="closed"
+              initial="closed"
+              style={{ originY: 'bottom' }}
+              transition={{
+                ...TRANS_SPRING,
+                filter: { delay: 0.2 },
+              }}
+              variants={{
+                closed: {
+                  filter: 'blur(4px)',
+                  opacity: 0,
+                  rotate: 20,
+                  scale: 0.2,
+                },
+                open: {
+                  filter: 'blur(0px)',
+                  opacity: 1,
+                  rotate: 0,
+                  scale: 1,
+                },
+              }}
+            >
+              <Tabs.Content value={TABS[0].label}>
+                <Cards
+                  ccv={ccv}
+                  overflow={overflow}
+                  setCcv={setCcv}
+                  setOverflow={setOverflow}
+                  setSwitchCard={setSwitchCard}
+                  switchCard={switchCard}
+                />
+                <footer
                   className={cn(
-                    'grid size-8 cursor-default place-content-center rounded-lg border border-transparent transition-colors',
-                    'border-zinc-200 hover:bg-zinc-100 hover:text-zinc-800',
-                    'dark:border-zinc-700/70 dark:text-zinc-400 dark:hover:bg-zinc-800 dark:hover:text-zinc-100',
-                  )}
-                  onClick={() => setSwitchCard(true)}
-                >
-                  <ArrowRightLeft className="size-4" />
-                </button>
-                <div
-                  className={cn(
-                    'relative flex overflow-hidden rounded-lg before:pointer-events-none before:absolute before:inset-0 before:z-10 before:rounded-lg before:shadow-pop-sm',
+                    'flex w-full items-center justify-between gap-2 pt-4',
                   )}
                 >
-                  <input
-                    className={cn(
-                      'w-[3.375rem] rounded-lg rounded-e-none border-r pl-3 pt-[2px] font-cc uppercase outline-0 transition-colors',
-                      'border-r-transparent bg-sky-100 text-sky-800 placeholder-sky-800/50 hover:bg-sky-200 focus-visible:bg-sky-100',
-                      'dark:border-r-black/40 dark:bg-sky-950/70 dark:text-sky-300 dark:placeholder-sky-300/40 dark:hover:bg-sky-950/80 dark:focus-visible:bg-sky-950/70',
-                    )}
-                    maxLength={3}
-                    onChange={e => setCcv(e.target.value)}
-                    pattern="\d*"
-                    placeholder="ccv"
-                    ref={inputRef}
-                    value={ccv}
-                  />
                   <button
+                    aria-label="Switch active credit card"
                     className={cn(
-                      'relative cursor-default whitespace-nowrap rounded-lg rounded-s-none px-3 py-2 outline-0 transition-colors',
-                      'z-0 after:absolute after:bottom-px after:left-0 after:top-px after:border-l',
-                      'bg-sky-100 text-sky-800 after:border-l-black/10 hover:bg-sky-200 focus-visible:bg-sky-200',
-                      'dark:bg-sky-950 dark:text-sky-300 dark:after:border-l-white/5 dark:hover:bg-sky-900/60 dark:hover:text-sky-200',
-                      'dark:focus-visible:bg-sky-900/60 dark:focus-visible:text-sky-200',
+                      'grid size-8 cursor-default place-content-center rounded-lg border border-transparent transition-colors',
+                      'border-zinc-200 hover:bg-zinc-100 hover:text-zinc-800',
+                      'dark:border-zinc-700/70 dark:text-zinc-400 dark:hover:bg-zinc-800 dark:hover:text-zinc-100',
+                    )}
+                    onClick={() => setSwitchCard(true)}
+                  >
+                    <ArrowRightLeft className="size-4" />
+                  </button>
+                  <div
+                    className={cn(
+                      'relative flex overflow-hidden rounded-lg before:pointer-events-none before:absolute before:inset-0 before:z-10 before:rounded-lg before:shadow-pop-sm',
                     )}
                   >
-                    Pay Now
-                  </button>
-                </div>
-              </footer>
-            </Tabs.Content>
-            <Tabs.Content value={TABS[1].label}>Other methods</Tabs.Content>
-          </m.div>
-        )}
+                    <input
+                      className={cn(
+                        'w-[3.375rem] rounded-lg rounded-e-none border-r pl-3 pt-[2px] font-cc uppercase outline-0 transition-colors',
+                        'border-r-transparent bg-sky-100 text-sky-800 placeholder-sky-800/50 hover:bg-sky-200 focus-visible:bg-sky-100',
+                        'dark:border-r-black/40 dark:bg-sky-950/70 dark:text-sky-300 dark:placeholder-sky-300/40 dark:hover:bg-sky-950/80 dark:focus-visible:bg-sky-950/70',
+                      )}
+                      maxLength={3}
+                      onChange={e => setCcv(e.target.value)}
+                      pattern="\d*"
+                      placeholder="ccv"
+                      ref={inputRef}
+                      value={ccv}
+                    />
+                    <button
+                      className={cn(
+                        'relative cursor-default whitespace-nowrap rounded-lg rounded-s-none px-3 py-2 outline-0 transition-colors',
+                        'z-0 after:absolute after:bottom-px after:left-0 after:top-px after:border-l',
+                        'bg-sky-100 text-sky-800 after:border-l-black/10 hover:bg-sky-200 focus-visible:bg-sky-200',
+                        'dark:bg-sky-950 dark:text-sky-300 dark:after:border-l-white/5 dark:hover:bg-sky-900/60 dark:hover:text-sky-200',
+                        'dark:focus-visible:bg-sky-900/60 dark:focus-visible:text-sky-200',
+                      )}
+                    >
+                      Pay Now
+                    </button>
+                  </div>
+                </footer>
+              </Tabs.Content>
+              <Tabs.Content value={TABS[1].label}>Other methods</Tabs.Content>
+            </m.div>
+          )}
+        </AnimatePresence>
       </Tabs.Root>
     </AnimateDimension>
   );
