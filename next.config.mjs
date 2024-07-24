7434e1e9f5e5cc7a6584387b893260d19c7edb66
Joonas Sandell <me@joonassandell.com>
2024-07-24 18:33:00 +0300

    This isn't needed, there's no scoping locally

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config) {
-    /**
-     * Related to tailwindcss-scoped-preflight causing issues together with 
-     * tailwinds resolveConfig
-     * 
-     * @link https://stackoverflow.com/a/70995196
-     */
-    config.resolve.fallback = {
-      ...config.resolve.fallback,  
-      fs: false,
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
