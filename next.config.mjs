5bdf1db2c04bdaab18d1dc96063c8500a6ea8c42
Joonas Sandell <me@joonassandell.com>
2024-07-26 00:53:04 +0300

    Add preview/build with ignores for local testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+const { IGNORE_ERRORS } = process.env;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: IGNORE_ERRORS ? true : false,
+  },
+  typescript: {
+    ignoreBuildErrors: IGNORE_ERRORS ? true : false,
+  },
+};
 
 export default nextConfig;
