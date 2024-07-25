bfb6a3c501bdd42b51c47f8a50ffddf1dc75c2d7
Joonas Sandell <me@joonassandell.com>
2024-07-25 19:15:11 +0300

    Add calmer animation, center exit animation

--- app/dynamic-pay-button/components/Content.tsx
+++ app/dynamic-pay-button/components/Content.tsx
@@ -2,36 +2,52 @@ import { AnimatePresence, m } from 'framer-motion';
 import { cn } from '@/lib/utils';
 import { type ContentProps, useDynamicPayButton } from '../';
 import { TRANS_SPRING } from '@/lib/config';
+import { useEffect, useState } from 'react';
 
 export const Content = ({ children }: ContentProps) => {
   const { open } = useDynamicPayButton();
+  const [present, setPresent] = useState(open);
+
+  useEffect(() => {
+    if (open) setPresent(true);
+  }, [open]);
+
+  if (!present) {
+    return null;
+  }
 
   return (
-    <AnimatePresence mode="popLayout">
-      {open && (
-        <m.div
-          animate="open"
-          className={cn('ul-w-[308px] ul-p-3 sm:ul-w-96', {
-            'ul-absolute ul-top-14': !open,
-          })}
-          exit="closed"
-          initial="closed"
-          style={{ originY: 'bottom' }}
-          transition={TRANS_SPRING}
-          variants={{
-            closed: {
-              rotate: 20,
-              scale: 0.2,
-            },
-            open: {
-              rotate: 0,
-              scale: 1,
-            },
-          }}
-        >
-          {children}
-        </m.div>
-      )}
-    </AnimatePresence>
+    <div
+      className={cn('ul-w-[308px] ul-p-3 sm:ul-w-96', {
+        'ul-absolute': !open,
+      })}
+    >
+      <AnimatePresence
+        mode="popLayout"
+        onExitComplete={() => setPresent(false)}
+      >
+        {open && (
+          <m.div
+            animate="open"
+            exit="closed"
+            initial="closed"
+            style={{ originY: 'bottom' }}
+            transition={TRANS_SPRING}
+            variants={{
+              closed: {
+                filter: 'blur(4px)',
+                opacity: 0,
+              },
+              open: {
+                filter: 'blur(0px)',
+                opacity: 1,
+              },
+            }}
+          >
+            {children}
+          </m.div>
+        )}
+      </AnimatePresence>
+    </div>
   );
 };
