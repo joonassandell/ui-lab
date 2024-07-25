84e5f3e5293fbbec0aebb3e4c58b2b9592ae6433
Joonas Sandell <me@joonassandell.com>
2024-07-25 11:47:08 +0300

    Add lib watch, fix prettier

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  clean: true,
-  dts: true,
-  entry: ['app/index.ts'],
-  format: 'esm',
-  minify: true,
+export default defineConfig(options => {
+  return {
+    clean: !options.watch,
+    dts: true,
+    entry: ['app/index.ts'],
+    format: 'esm',
+    minify: !options.watch,
+  };
 });
