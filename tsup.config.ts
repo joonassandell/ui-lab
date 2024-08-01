d6b56ac67e63d3b2375ba87f7e1570177e0c7208
Joonas Sandell <me@joonassandell.com>
2024-08-02 00:17:48 +0300

    Might as well use esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig(options => {
     clean: !options.watch,
     dts: true,
     entry: ['app/index.ts'],
-    format: 'cjs',
+    format: 'esm',
     minify: !options.watch,
     target: 'node20',
   };
