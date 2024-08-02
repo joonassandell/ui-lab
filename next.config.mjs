92c9e6ce8d9d332c50510b4dfd889e22e899c9dc
Joonas Sandell <me@joonassandell.com>
2024-08-02 12:57:20 +0300

    Allow iframe usage in whitelisted urls only

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const { IGNORE_ERRORS } = process.env;
+const { IGNORE_ERRORS, NEXT_PUBLIC_ALLOWED_IFRAME_URLS } = process.env;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,6 +7,19 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: IGNORE_ERRORS ? true : false,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `frame-ancestors 'self' ${NEXT_PUBLIC_ALLOWED_IFRAME_URLS.split(', ').join(' ')}`,
+          },
+        ],
+      },
+    ];
   },
 };
 
